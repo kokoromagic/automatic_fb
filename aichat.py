2343f34e28f6a9a31927695c1ac541e8c983c992
HuskyDG <84650617+HuskyDG@users.noreply.github.com>
2025-02-19 14:21:08 +0700

    chore: remove annoying typing...

--- aichat.py
+++ aichat.py
@@ -375,13 +375,6 @@ try:
                             )
                         except Exception as e:
                             print_with_time(e)
-
-                        try:
-                            button = get_message_input()
-                            driver.execute_script("arguments[0].click();", button)
-                            get_message_input().send_keys(" ")
-                        except Exception:
-                            pass
                         
                         try:
                             profile_btn = driver.find_elements(By.CSS_SELECTOR, 'a[class="x1i10hfl x1qjc9v5 xjbqb8w xjqpnuy xa49m3k xqeqjp1 x2hbi6w x13fuv20 xu3j5b3 x1q0q8m5 x26u7qi x972fbf xcfux6l x1qhh985 xm0m39n x9f619 x1ypdohk xdl72j9 xe8uvvx xdj266r x11i5rnm xat24cr x1mh8g0r x2lwn1j xeuugli xexx8yu x4uap5 x18d9i69 xkhd6sd x1n2onr6 x16tdsg8 x1hl2dhg xggy1nq x1ja2u2z x1t137rt x1o1ewxj x3x9cwd x1e5q0jg x13rtm0m x1q0g3np x87ps6o x1lku1pv x1rg5ohu x1a2a7pz xs83m0k"]')
@@ -515,12 +508,13 @@ try:
                             header_prompt = get_header_prompt(get_day_and_time(), who_chatted, facebook_info)
 
                             prompt_list.append(f'The Messenger conversation with "{who_chatted}" is as json here:')
-                            try:
-                                button = get_message_input()
-                                driver.execute_script("arguments[0].click();", button)
-                                get_message_input().send_keys(" ")
-                            except Exception:
-                                pass
+                            if not is_group_chat:
+                                try:
+                                    button = get_message_input()
+                                    driver.execute_script("arguments[0].click();", button)
+                                    get_message_input().send_keys(" ")
+                                except Exception:
+                                    pass
 
                             print_with_time("Đang đọc tin nhắn...")
 
